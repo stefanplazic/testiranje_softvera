c1689d2e4514e102a359bc2a8b1169f86fa054db
sirko <diskozub@hotmail.com>
2016-12-19 13:52:32 +0100

    advertiser i estate kontroler metode sad koriste token za prepoznavanje korisnika

--- src/main/java/com/nekretnine/controllers/AdvertisementController.java
+++ src/main/java/com/nekretnine/controllers/AdvertisementController.java
@@ -1,5 +1,7 @@
 package com.nekretnine.controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nekretnine.dto.AdvertisementDTO;
 import com.nekretnine.models.Advertisement;
+import com.nekretnine.models.Advertiser;
 import com.nekretnine.services.AdvertisementService;
+import com.nekretnine.services.UserService;
 
 @RestController
 @RequestMapping(value="api/advertisement")
@@ -20,12 +24,16 @@ public class AdvertisementController {
 	@Autowired
 	private AdvertisementService advertisementService;
 	
+	@Autowired
+	private UserService userService;
+	
 	//za oglasivaca
 	@RequestMapping(value="/add",method=RequestMethod.POST,consumes="application/json")
-	public ResponseEntity<String> add_advertisement(@RequestBody AdvertisementDTO advertisementDTO){
+	public ResponseEntity<String> add_advertisement(Principal principal,@RequestBody AdvertisementDTO advertisementDTO){
 		
-		
-		Advertisement a=new Advertisement(advertisementDTO);		
+		Advertiser u=(Advertiser)userService.findByUsername(principal.getName());		
+		Advertisement a=new Advertisement(advertisementDTO);
+		a.setAdvertiser(u);
 		advertisementService.save(a);
 		
 		return new ResponseEntity<String>("aloebebebebe",HttpStatus.OK);
@@ -33,14 +41,15 @@ public class AdvertisementController {
 	
 	//za oglasivaca
 	@RequestMapping(value="/modify/{advert_id}",method=RequestMethod.POST,consumes="application/json")
-	public ResponseEntity<String> modify_addvertisement(@PathVariable Long advert_id,@RequestBody AdvertisementDTO advertDTO){
+	public ResponseEntity<String> modify_addvertisement(Principal principal,@PathVariable Long advert_id,@RequestBody AdvertisementDTO advertDTO){
 		
 		Advertisement adv = advertisementService.findOne(advert_id);
+		Advertiser u=(Advertiser)userService.findByUsername(principal.getName());		
 	
 		//da li reklama postoji
 		if(adv==null) return new ResponseEntity<String>("nemanema",HttpStatus.OK);
 		//da li je oglasivac okacio reklamu
-		if(adv.getAdvertiser().getId()!=advertDTO.getAdvertiser().getId())return new ResponseEntity<String>("nemoze",HttpStatus.OK);
+		if(adv.getAdvertiser().getId()!=u.getId())return new ResponseEntity<String>("nemoze",HttpStatus.OK);
 		
 		//apdejt status reklame ako je prosledjen
 		if(advertDTO.getState()!=null) advertisementService.setState(advertDTO.getState(),advert_id);
@@ -51,19 +60,20 @@ public class AdvertisementController {
 	}
 	
 	//za oglasivaca
-	@RequestMapping(value="/delete/{advert_id}",method=RequestMethod.POST,consumes="application/json")
-	public ResponseEntity<String> delete_advertisement(@PathVariable Long advert_id,@RequestBody AdvertisementDTO advertDTO ){
+	@RequestMapping(value="/delete/{advert_id}",method=RequestMethod.DELETE,consumes="application/json")
+	public ResponseEntity<String> delete_advertisement(Principal principal,@PathVariable Long advert_id ){
 		
+		Advertiser u=(Advertiser)userService.findByUsername(principal.getName());	
 		Advertisement adv = advertisementService.findOne(advert_id);
 		
 		//da li reklama postoji
 		if(adv==null) return new ResponseEntity<String>("nemanema",HttpStatus.OK);
 		//da li je oglasivac okacio reklamu
-		if(adv.getAdvertiser().getId()!=advertDTO.getAdvertiser().getId()) return new ResponseEntity<String>("nemoze",HttpStatus.OK);
+		if(adv.getAdvertiser().getId()!=u.getId()) return new ResponseEntity<String>("nemoze",HttpStatus.OK);
 			
-		System.out.println("asd");
+		
 		advertisementService.delete(advert_id);
-		System.out.println("asd");
+		
 		return new ResponseEntity<String>("brisnuto",HttpStatus.OK);
 	}
 	
