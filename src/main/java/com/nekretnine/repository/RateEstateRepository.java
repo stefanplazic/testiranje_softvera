74e3808f126457bcd77b9356dd4eaffd0b6699e4
sirko <diskozub@hotmail.com>
2016-12-21 13:58:25 +0100

    rejtovanje nekretnine Closed#35

---
+++ src/main/java/com/nekretnine/repository/RateEstateRepository.java
@@ -0,0 +1,9 @@
+package com.nekretnine.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nekretnine.models.RateEstate;
+
+public interface RateEstateRepository extends JpaRepository<RateEstate,Long> {
+
+}
