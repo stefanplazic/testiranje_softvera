0cb3341677207ac579440384444af1f3005a5c08
Stefan Plazić <stefan.plazic@gmail.com>
2016-12-21 22:20:22 +0100

    Odradjen UserServiceTest

--- src/test/java/com/nekretnine/constants/UserConstants.java
+++ src/test/java/com/nekretnine/constants/UserConstants.java
@@ -6,14 +6,25 @@ public class UserConstants {
 	public static final String USERNAME = "stefi";
 	public static final String PASSWORD = "plaza";
 	public static final String EMAIL = "stefi@gmail.com";
+	public static final boolean VERIFIED = false;
 	
 	public static final String FIRST_NAME_SECOND = "Mile";
 	public static final String LAST_NAME_SECOND = "Bambi";
 	public static final String USERNAME_SECOND = "tombola";
-	public static final String PASSWORD_SECOND = "mile";
+	public static final String PASSWORD_SECOND = "$2a$10$UZvCW6hqOqjnbz8xvuwlWOo5R2cuhRxt6nn6XXqJxsGC.SF.zJHmi";
 	public static final String EMAIL_SECOND = "mile@gmail.com";
 	
 	//this is for UserServiceTest
 	public static final int FIND_ALL_COUNT = 2;
 	public static final int PAGE_SIZE = 2;
+	
+	public static final Long ADMIN_ID = 1l;
+	public static final String ADMIN_USERNAME = "admin";
+	public static final String ADMIN_EMAIL = "admin";
+	public static final String ADMIN_FIRST_NAME = "admin";
+	public static final String ADMIN_LAST_NAME = "admin";
+	
+	public static final Long USER_ID= 2L;
+	public static final String VERIFY_CODE = "4dc9975b-3103-49c1-80b3-ee548d42d56a";
+	 
 }
