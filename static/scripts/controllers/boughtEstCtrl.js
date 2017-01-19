ea05638f4bb20de050ce83b7352115e233f44786
Stefan Plazić <stefan.plazic@gmail.com>
2017-01-19 18:30:08 +0100

    Minor bug fixes issue #50

--- static/scripts/controllers/boughtEstCtrl.js
+++ static/scripts/controllers/boughtEstCtrl.js
@@ -10,6 +10,7 @@
         var vm = this;
         vm.perPage = 2;//size of page
         vm.pages = [];
+        vm.getAdvert = getAdvert;
 		if ($cookies.getObject('userdata') === undefined)
 			$window.location="#/login";
 
@@ -21,12 +22,24 @@
 			var pageNum = Math.floor((parseInt(response.data.response) + vm.perPage -1) / vm.perPage) ;
 			for(var i=1;i <= pageNum; i++)
 				vm.pages.push(i);
-			console.log(vm.pages)
+			getAdvert(0);//call the function
 		}, function(error) {
 			// log error response and maybe send it to
 			// error monitor app
 			console.error("Error ocurred: " + response.status);
 		});
+		
+		/**
+		 * get advertisments for given page number
+		 * pNum - int
+		 */
+		function getAdvert(pNum){
+			var pageRequest = {"page": pNum, "count": vm.perPage};
+			 $http.post("/api/customer/myEstates", pageRequest, { headers: { 'X-Auth-Token': $cookies.get("token") } })
+			 	.then(function (response){
+			 		console.log(response.data)
+			 	});
+		}
     }
     
 })();
