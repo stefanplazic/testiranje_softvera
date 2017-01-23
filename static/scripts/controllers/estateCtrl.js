3fac8e5519c5078a9f77f5611d459d54868637ba
sirko <diskozub@hotmail.com>
2017-01-23 15:16:44 +0100

    estateview.2

--- static/scripts/controllers/estateCtrl.js
+++ static/scripts/controllers/estateCtrl.js
@@ -5,12 +5,21 @@
 	function estateController($routeParams,$cookies,$http) {
 		var vm=this;
 		vm.estate;
+		vm.rate;
 
 		$http.get('api/estate/'+$routeParams.id,{headers : {'X-Auth-Token' : $cookies.get("token")}}).then(function(response){
-			console.log(JSON.stringify(response.data))
+			
 			vm.estate=response.data;
 		})
 
+		vm.rateEstate=function(){
+
+			$http.post('api/estate/rate/'+$routeParams.id,{rate:vm.rate}
+				,{headers : {'X-Auth-Token' : $cookies.get("token")}}).then(function(response){
+
+					alert(JSON.stringify(response.data))
+				})
+		}
 
 	}
 
