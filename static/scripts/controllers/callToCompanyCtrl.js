b5118f76d43f8a58a8831fb8c6519ad89b772810
Stefan Plazić <stefan.plazic@gmail.com>
2017-01-23 16:25:50 +0100

    Bug fixes . see issue #52

--- static/scripts/controllers/callToCompanyCtrl.js
+++ static/scripts/controllers/callToCompanyCtrl.js
@@ -7,6 +7,7 @@
 		var vm = this;
 		vm.selected = undefined;
 		vm.typeSelect = typeSelect;
+		vm.getCompanyList = getCompanyList;
 		getCompany();
 
 		function getUnemployed() {
@@ -22,20 +23,64 @@
 		}
 
 		/**
-		 * this function is triggered every single time , when we select value in searchbox
+		 * this function is triggered every single time , when we select value
+		 * in searchbox
 		 */
-		function typeSelect($item, $model, $label) {console.log($item);}
+		function typeSelect($item, $model, $label) {
+			console.log($item);
+			sendRequest($item);//send data to server
+		}
+		/**
+		 * this function enables to see if user is working in any company
+		 */
+		function getCompany() {
+			$http.get("api/advertiser/getCompany", {
+				headers : {
+					'X-Auth-Token' : $cookies.get("token")
+				}
+			}).then(function(response) {
+				vm.hasCompany = true;// if true, show search bar to user
+				vm.myCompany = response.data;
+				getUnemployed();// call the population function
+			}, function(response) {
+				console.log("Not found company");
+				vm.hasCompany = false;
+			});
+		}
+
+		/**
+		 * send request to user to start working in company.
+		 */
+		function sendRequest(advertiser) {
+			var userData = {
+				"username" : vm.username,
+				"password" : vm.pass,
+				"email" : vm.email,
+				"firstName" : vm.firstName,
+				"lastName" : vm.lastName
+			};
+			$http.post('api/advertiser/callToCompany', advertiser,{ headers: { 'X-Auth-Token': $cookies.get("token") } })
+					.then(function(response) {
+						if (response) {
+							alert(response.data.response);
+						}
+					}, function(response) {
+						alert(response.data.response);
+					});
+		}
 		
-		function getCompany() {$http.get("api/advertiser/getCompany", {
-				headers : {'X-Auth-Token' : $cookies.get("token")}})
-				.then(function(response) {
-					vm.hasCompany = true;//if true, show search bar to user
-					vm.myCompany = response.data;
-					getUnemployed();//call the population function
-				}, 
-						function(response) {console.log("Not found company"); 
-				vm.hasCompany = false;		
-				});
+		/**
+		 * fetch list of all company invitations
+		 */
+		function getCompanyList(){
+			$http.post('api/advertiser/allCalls',{ headers: { 'X-Auth-Token': $cookies.get("token") } })
+			.then(function(response) {
+				if (response) {
+					console.log(response.data);
+				}
+			}, function(response) {
+				console.log("No company calls for you");
+			});
 		}
 
 	}
